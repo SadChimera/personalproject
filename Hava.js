95d6678e489e313bbb2e499e9fb84635dc213140
JenniferLee <jennifer.lee24@student.cart.org>
2025-02-03 10:39:36 -0800

    dfs

--- Hava.js
+++ Hava.js
@@ -1,2 +1,19 @@
-const myHeading = document.querySelector(p);
-myHeading.textContent = "Please, input your name:";
+let myButton = document.querySelector("button");
+let myParagraph = document.querySelector("p");
+function setUserName() {
+    const myName = prompt("Enter a name");
+    localStorage.setItem("name", myName);
+    myParagraph.textContent = `Enter a name, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+    setUserName();
+});
+function setUserName() {
+    const myName = prompt("Enter a name");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myParagraph.textContent = `Enter a name, ${myName}`
+    }
+}
